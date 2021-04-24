684902d0156b2626388af970f13bebf90e5b5e89
httpjamesm <github@httpjames.space>
2021-04-24 14:57:18 -0400

    cat slots in help menu

--- cogs/help.py
+++ cogs/help.py
@@ -17,7 +17,7 @@ class help(commands.Cog):
         embed=discord.Embed(title=f"{title}", description=f"{desc}", color=0x32e6ff)
         embed.set_author(name=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=f"{ctx.author.avatar_url}")
         embed.set_thumbnail(url="https://user-images.githubusercontent.com/7556992/40873750-8065e8e6-6690-11e8-9417-69fd6e759c9b.png")
-        embed.add_field(name="Slots", value=f"`{prefix}fs` - Fruit slots\n`{prefix}pps` - Pepe slots\n`{prefix}pog` - Pog slots\n`{prefix}vpn` - VPN slots", inline=False)
+        embed.add_field(name="Slots", value=f"`{prefix}fs` - Fruit slots\n`{prefix}pps` - Pepe slots\n`{prefix}pog` - Pog slots\n`{prefix}vpn` - VPN slots\n`{prefix}cat` - Cat slots", inline=False)
         embed.add_field(name="Rolls", value=f"`{prefix}cr` - Cat roll", inline=False)
         embed.add_field(name="Guess Games", value=f"`{prefix}gg10 <guess>` - 1/10\n`{prefix}ggd <guess>` - 1-51 with decimals\n`{prefix}ggi <guess>` - 1/1000", inline=False)
         embed.add_field(name="Contribute", value="Are you a developer? Contribute code to the project at the [git repo](https://github.com/httpjamesm/CasinoScribe).", inline=False)
