ba0c3867b2ca0ef01b18ff70b3dd8f094d65920d
httpjamesm <github@httpjames.space>
2021-04-24 01:50:44 -0400

    link repo in help

--- cogs/help.py
+++ cogs/help.py
@@ -7,7 +7,7 @@ class help(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         # Re-define the bot object into the class.
-        
+
     @commands.cooldown(1,10,commands.BucketType.guild)
     @commands.command()
     async def help(self, ctx):
@@ -19,6 +19,7 @@ class help(commands.Cog):
         embed.set_thumbnail(url="https://user-images.githubusercontent.com/7556992/40873750-8065e8e6-6690-11e8-9417-69fd6e759c9b.png")
         embed.add_field(name="Slots", value=f"`{prefix}fs` - Fruit slots\n`{prefix}pps` - Pepe slots\n`{prefix}pog` - Pog slots\n`{prefix}vpn` - VPN slots", inline=False)
         embed.add_field(name="Rolls", value=f"`{prefix}cr` - Cat roll", inline=False)
+        embed.add_field(name="Contribute", value="Are you a developer? Contribute code to the project at the [git repo](https://github.com/httpjamesm/CasinoScribe)", inline=False)
         embed.set_footer(text="Made by http.james#6969")
         await ctx.send(embed=embed)
 
